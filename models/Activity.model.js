e7632f7b67f2ba26fa141cca7eac6218c6198f69
Fabio García <fabiogcaam@gmail.com>
2024-09-30 19:43:27 +0200

    activities model

---
+++ models/Activity.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose")
+
+const activitySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required']
+        },
+        description: {
+            type: String,
+            required: [true, 'Description is required']
+        },
+        imageUrl: {
+            type: String
+        }
+    })
