e0d9c3e822e036671fa55b87e1f7ea2db95931ea
Fabio García <fabiogcaam@gmail.com>
2024-10-01 13:36:03 +0200

    added maximum number of student per class at class model

--- models/Class.model.js
+++ models/Class.model.js
@@ -12,6 +12,20 @@ const classSchema = new Schema({
     schedule: {
         type: Array,
         required: [true, 'Schedule is required']
+    },
+    participants: {
+        type: [Schema.Types.ObjectId],
+        ref: 'User',
+        validate: {
+            validator: function (v) {
+                return v.length <= this.numParticipants
+            },
+            props: `It must have a maximum of ${numParticipants} per class`
+        }
+    },
+    numParticipants: {
+        type: Number,
+        required: [true, 'Max number of participants ir required']
     }
 })
 
