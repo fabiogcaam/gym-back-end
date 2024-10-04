f1e4606462eace0c570ca69829ec24dddaed38b9
Fabio García <fabiogcaam@gmail.com>
2024-10-04 14:27:47 +0200

    user controller change

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
-const { bookingList } = require('./../controllers/user.controller')
+const { bookingList, deleteBooking } = require('./../controllers/user.controller')
 const { verifyToken } = require('./../middlewares/verifyToken')
 
 router.get('/bookings', verifyToken, bookingList)
+
+router.post('/deleteBooking', verifyToken, deleteBooking)
