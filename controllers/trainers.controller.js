00f163a4d8d28d6a7e00fbb025a237be3656c1e9
Fabio García <fabiogcaam@gmail.com>
2024-10-04 18:23:27 +0200

    create and add Activity, add Class on trainers

--- controllers/trainers.controller.js
+++ controllers/trainers.controller.js
@@ -9,6 +9,45 @@ function getTrainers(req, res, next) {
         .catch(err => next(err))
 }
 
+function createTrainer(req, res, next) {
+
+    const { name, age, description, imageUrl } = req.body
+
+
+    Trainer
+        .create({ name, age, description, imageUrl })
+        .then(() => res.status(201).json('Trainer created succesfylly'))
+        .catch(err => next(err))
+
+}
+
+function addClassToTrainer(req, res, next) {
+
+    const { idClass } = req.params
+
+    const { loggedUser } = req.payload
+
+    Trainer.findByIdAndUpdate(loggedUser, { $push: { class: idClass } }, { new: true }).populate('activity').populate('class')
+        .then((trainerUpdated) => res.status(202).json(trainerUpdated))
+        .catch(err => next(err))
+
+}
+
+function addActivityToTrainer(req, res, next) {
+
+    const { idActivity } = req.params
+    const { loggedUser } = req.payload
+
+    Trainer.findByIdAndUpdate(loggedUser, { $push: { activity: idActivity } }, { new: true }).populate('activity').populate('class')
+        .then((trainerUpdated) => res.status(202).json(trainerUpdated))
+        .catch(err => next(err))
+
+}
+
+
 module.exports = {
-    getTrainers
+    getTrainers,
+    createTrainer,
+    addClassToTrainer,
+    addActivityToTrainer
 }
--- routes/trainers.routes.js
+++ routes/trainers.routes.js
@@ -1,4 +1,10 @@
 const router = require('express').Router()
-const { getTrainers } = require('./../controllers/trainers.controller')
+const { getTrainers, createTrainer, addActivityToTrainer, addClassToTrainer } = require('./../controllers/trainers.controller')
 
 router.get('/', getTrainers)
+
+router.post('/create', createTrainer)
+
+router.post('/addActivity', addActivityToTrainer)
+
+router.post('/addClass', addClassToTrainer)
